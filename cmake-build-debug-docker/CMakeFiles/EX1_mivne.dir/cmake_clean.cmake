bf7f6b5f4c16f690f955a70ba20dbd94f18e3f26
funkey7dan <72495653+funkey7dan@users.noreply.github.com>
2021-10-23 13:56:39 +0300

    Finished.

---
+++ cmake-build-debug-docker/CMakeFiles/EX1_mivne.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EX1_mivne.dir/ex1.c.o"
+  "CMakeFiles/EX1_mivne.dir/main.c.o"
+  "EX1_mivne"
+  "EX1_mivne.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EX1_mivne.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
